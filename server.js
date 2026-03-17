baba66fbf40d72472d36997a40c1110fff6abb9e
Maratonx8 <lelearu14@gmail.com>
2026-03-17 18:17:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,20 +2,35 @@ const express = require("express")
 const cors = require("cors")
 
 const app = express()
-
 app.use(cors())
 app.use(express.json())
 
 let connections = {}
 
 app.get("/", (req,res)=>{
-    res.json({ok:true, service:"LazyMan API"})
+    res.json({ok:true})
+})
+
+app.post("/plugin/connect-code", (req,res)=>{
+    const code = "LM-" + Math.floor(Math.random()*90000+10000)
+
+    connections[code] = {
+        connected:false,
+        lastPing:0
+    }
+
+    res.json({ok:true, code})
 })
 
 app.post("/plugin/connect", (req,res)=>{
     const { code } = req.body
 
-    connections[code] = Date.now()
+    if(!connections[code]){
+        return res.json({ok:false})
+    }
+
+    connections[code].connected = true
+    connections[code].lastPing = Date.now()
 
     res.json({ok:true})
 })
@@ -23,7 +38,9 @@ app.post("/plugin/connect", (req,res)=>{
 app.post("/plugin/ping", (req,res)=>{
     const { code } = req.body
 
-    connections[code] = Date.now()
+    if(connections[code]){
+        connections[code].lastPing = Date.now()
+    }
 
     res.json({ok:true})
 })
@@ -31,17 +48,17 @@ app.post("/plugin/ping", (req,res)=>{
 app.get("/plugin/status", (req,res)=>{
     const code = req.query.code
 
-    const last = connections[code]
+    const c = connections[code]
 
-    if(!last){
+    if(!c){
         return res.json({connected:false})
     }
 
-    const alive = Date.now() - last < 15000
+    const alive = Date.now() - c.lastPing < 15000
 
     res.json({connected: alive})
 })
 
 app.listen(3000, ()=>{
-    console.log("LazyMan API running")
+    console.log("LazyMan API RUNNING")
 })
